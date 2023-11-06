0eb2c4fc2110f985b3e6e2548fb3d222da24b5ce
Adrian Grociak <ajgrociak@gmail.com>
2023-11-06 16:09:03 +0100

    Change initActions() so the listner is set for booksList container instead of single image

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,9 @@
     book: Handlebars.compile(document.querySelector('#template-book').innerHTML),
   };
 
+  const booksList = document.querySelector('.books-list');
+  console.log(booksList);
+  
   const favoriteBooks = [];
 
   function renderBooks() {
@@ -25,12 +28,46 @@
   }
 
   function initActions() {
+
     const bookImgs = document.querySelectorAll('.book__image');
     console.log(bookImgs);
+    
 
-    for (let singleBook of bookImgs) {
+    booksList.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      console.log(event);
 
-      const singleBookDataId = singleBook.getAttribute('data-id');
+      const clickedBook = event.target.closest('a');
+      console.log(clickedBook);
+
+      const bookId = clickedBook.getAttribute('data-id');
+      console.log(bookId);
+
+      if (!favoriteBooks.includes(bookId)) {
+        
+        clickedBook.classList.add('favorite');
+
+        favoriteBooks.push(bookId);
+
+        console.log(favoriteBooks);
+
+      } else {
+
+        let indexOf = favoriteBooks.indexOf(bookId);
+
+        favoriteBooks.splice(indexOf, 1);
+
+        clickedBook.classList.remove('favorite');
+
+        console.log(favoriteBooks);
+        
+      }
+
+    });
+
+    /* for (let singleBook of bookImgs) {
+
+      const bookId = singleBook.getAttribute('data-id');
       console.log(singleBook);   
 
       singleBook.addEventListener('dblclick', function() {
@@ -41,12 +78,13 @@
 
           singleBook.classList.add('favorite');
 
-          favoriteBooks.push(singleBookDataId);
+          favoriteBooks.push(bookId);
 
           console.log(favoriteBooks);
+
         } else {
 
-          let indexOf = favoriteBooks.indexOf(singleBookDataId);
+          let indexOf = favoriteBooks.indexOf(bookId);
 
           singleBook.classList.remove('favorite');
 
@@ -54,15 +92,11 @@
 
         }
 
-      });
-    }
-
+      }); */
   }
-
 
   renderBooks();
 
   initActions();
-
 
 }
